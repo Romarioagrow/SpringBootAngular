78662a95a674b682ad639458cff082f1f05eade7
Romarioagrow <romarioagrow@mail.ru>
2021-03-13 19:46:20 +0200

    Products App

--- src/main/resources/frontend/angular-app/src/app/products/products.component.ts
+++ src/main/resources/frontend/angular-app/src/app/products/products.component.ts
@@ -1,8 +1,5 @@
-/*
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Product} from "../product";
 
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -10,92 +7,8 @@ import {Product} from "../product";
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor(private http: HttpClient) {
-    //this.usersUrl = 'http://localhost:8080/users';
-  }
-
+  constructor(){}
 
   ngOnInit(): void {
-    console.log('ngOnInit')
-
-    this.findAll()
-
   }
-
-  public findAll(): any {
-    const url = 'http://localhost:9000/api/products/get/all'
-    return this.http.get<Product[]>(url).subscribe(value => {
-      console.log(value)
-      console.log(value)
-    });
-  }
-
-}
-
-
-
-
-*/
-
-
-
-
-import {Component, isDevMode, OnInit} from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import {Product} from "../product";
-
-@Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
-})
-export class ProductsComponent implements OnInit {
-  constructor(/*private http: HttpClient*/){}
-
-  /*displayedColumns: string[] = ['productID', 'productName', 'productType', 'productPrice'];
-  dataSource :  Object = [];*/
-
-  ngOnInit(): void {
-
-   // this.findAll();
-
-    /*let apiUrl = 'api/products'
-    let url = isDevMode() ? 'http://localhost:9000/app-cli/' + apiUrl : apiUrl
-    console.log('let url: ' + url)
-
-    this.http.get(url,{
-      headers: {'Access-Control-Allow-Origin':'*'}
-    }).subscribe(data => {
-      console.log(data);
-      if (data) {
-        this.dataSource = data;
-      }
-    });*/
-  }
-
-  /*public findAll(): any {
-    const url = 'http://localhost:9000/api/products/get/all'
-    return this.http.get<Product[]>(url).subscribe(value => {
-      console.log(value)
-      console.log(value)
-    });
-  }
-
-  createProductsList() {
-    console.log('createProductsList')
-    let apiUrl = 'api/products/createList'
-    let url = isDevMode() ? 'http://localhost:8080/app-cli/' + apiUrl : apiUrl
-    console.log('createProductsList url: ' + url)
-    this.http.get(url,{
-      headers: {'Access-Control-Allow-Origin':'*'}
-    }).subscribe(data => {
-      console.log(data);
-      if (data) {
-        console.log(data)
-        this.dataSource = data
-      }
-    });
-  }*/
-
 }
